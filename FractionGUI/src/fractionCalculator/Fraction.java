e9595be7d3aad8a04cce4ece02afe408237c2e9c
Nathan Schmidt <Nathan Schmidt@192.168.87.45>
2019-09-11 06:57:21 -0400

    9/10/2019

--- FractionGUI/src/fractionCalculator/Fraction.java
+++ FractionGUI/src/fractionCalculator/Fraction.java
@@ -4,90 +4,83 @@ public class Fraction {
 
 	public int numerator;
 	public int denominator;
-	
-	public Fraction(int num, int den){
+
+	public Fraction(int num, int den) {
 		numerator = num;
 		denominator = den;
+		
+		
 	}
-	
-	public Fraction(){
+
+	public Fraction() {
 		numerator = 0;
 		denominator = 0;
 	}
-	
-	
+
 	public String reduce() {
-		int whole =0;
-		int num =numerator;
-		int den = denominator;
+		Boolean negative = numerator * denominator < 0;
+		int num = Math.abs(numerator);
+		int den = Math.abs(denominator);
+		int whole;
 		String str = new String();
+
 		
-		if (num >0 && den >0) {
-			for (int i = Math.min(num, den); i>0; i--){
-				if (num %i == 0 && den%i == 0 ) {
-					num = num /i;
-					den = den /i;
+		
+		
+		if (num > 0 && den > 0) {
+			for (int i = Math.min(num, den); i > 0; i--) {
+				if (num % i == 0 && den % i == 0) {
+					num = num / i;
+					den = den / i;
 				}
 			}
 		}
-		
-		else if (num <0 && den <0) {
-			for (int i = Math.min(num, den); i<0; i++){
-				if (num %i == 0 && den%i == 0 ) {
-					num = num /i;
-					den = den /i;
-				}
+		whole = num / den;
+		num = num - (whole * den);
+
+		//add sign back in
+		if (negative) {
+			if (whole>0) {
+				whole = -whole;
+			}
+			else {
+				num = -num;
 			}
 		}
-		
-		whole = num/den;
-		num = num - (whole*den);
 		
 		if (whole != 0) {
 			str = String.format("%d ", whole);
 		}
-		
-		if(num != 0) {
+
+		if (num != 0) {
 			str += String.format("%d/%d", num, den);
 		}
-		
+
 		return str;
-		
+
 	}
-	
-	
-	
-	//Negative
-	public void negativeFrac () {
-		if (numerator<0 && denominator<0) {
-			numerator = Math.abs(numerator);
-			denominator = Math.abs(denominator);
-		}
-	}
-	
-	
-	public void multiply (Fraction f1, Fraction f2) {
+
+
+
+	// Multiply
+	public void multiply(Fraction f1, Fraction f2) {
 		
 		numerator = f1.numerator * f2.numerator;
 		denominator = f1.denominator * f2.denominator;
-		
+
 	}
-	
-	public String toString (boolean reduce) {
-		
+
+	public String toString(boolean reduce) {
+
 		if (reduce) {
-			
+
 			return reduce();
-		}
-		else {
+		} else {
 			return String.format("%d/%d", numerator, denominator);
 		}
-		
+
 	}
-	
-	
+
 }
-
-
 
 
--- FractionGUI/src/fractionCalculator/FractionUI.java
+++ FractionGUI/src/fractionCalculator/FractionUI.java
@@ -1,96 +1,97 @@
 package fractionCalculator;
+
 import javax.swing.*;
 import BreezySwing.*;
 
-public class FractionUI extends GBFrame{
-	
-    JLabel num1							= addLabel 			("First Fraction",			1,1,1,1);
-    IntegerField num1Field			 	= addIntegerField 	(0,     	      			1,2,1,1);
-    JLabel den1							= addLabel 			("/", 						1,3,1,1);
-    IntegerField den1Field			 	= addIntegerField 	(0,     	      			1,4,1,1);
-   
-    JLabel num2				        	= addLabel			("Second Fraction",   	 	2,1,1,1);
-    IntegerField num2Field			   	= addIntegerField 	(0,  	          			2,2,1,1);
-    JLabel den2				        	= addLabel			("/", 		   				2,3,1,1);
-    IntegerField den2Field			   	= addIntegerField 	(0,    	        			2,4,1,1);
-    
-    
-    JButton calculate	              	= addButton 		("calculate",            	3,1,1,1);
-    
-    	    
-    public void buttonClicked(JButton buttonObj){
-        int den1, num1, den2, num2;
-        
-        Fraction f1,f2, f3;
-        String str = "";
-        
-      
-        if (buttonObj == calculate){
-        	//inputs
-            num1 = num1Field.getNumber();
-            den1 = den1Field.getNumber();
-            num2 = num2Field.getNumber();
-            den2 = den2Field.getNumber();
-            
-            
-            //Error Checking
-            String error = "ERROR:";
-            String zero = "";
-            String noNum = "";
-	        Boolean errorCheck = true; 
-            
-            while (errorCheck = true) {
-	            if(den1 == 0 || den2 == 0 ) {
-	            	zero = "The denominator cannot be Zero";
-	            }
-	            else {
-	            	errorCheck = false;
-	            	break;
-	            }
-	 
-	            
-	            
-	            
-	            
-	          
-	        error = String.format("%s \n %s", zero, noNum );
-	        messageBox(error);
-		    break;
-            }
-            
-            //create fractions
-            
-            f1 = new Fraction(num1, den1);
-            f2 = new Fraction(num2, den2);
-            
-            //Negatives
-            f1.negativeFrac();
-            f2.negativeFrac();
-            
-            //calculations
-            
-            f3 = new Fraction();
-            f3.multiply(f1, f2);
-            
-            str=String.format("%s * %s = %s", f1.toString(true), f2.toString(true), f3.toString(true));
-            
-            messageBox(str);
-        }
+public class FractionUI extends GBFrame {
 
-    }
+	JLabel num1 = addLabel										("First Fraction",		 1, 1, 1, 1);
+	IntegerField num1Field = addIntegerField					(0,						 1, 2, 1, 1);
+	JLabel den1 = addLabel										("/",					 1, 3, 1, 1);
+	IntegerField den1Field = addIntegerField					(0, 					 1, 4, 1, 1);
 
-    public static void main(String[] args){
-        JFrame frm = new FractionUI();
-        frm.setTitle ("Fraction Calculator");
-        frm.setSize (500, 300);
-        frm.setVisible (true);
-   }
+	JLabel num2 = addLabel										("Second Fraction",		 2, 1, 1, 1);
+	IntegerField num2Field = addIntegerField					(0,						 2, 2, 1, 1);
+	JLabel den2 = addLabel										("/",					 2, 3, 1, 1);
+	IntegerField den2Field = addIntegerField					(0,						 2, 4, 1, 1);
 
+	JButton multiply = addButton								("multiply",			 3, 1, 1, 1);
+
+	public void buttonClicked(JButton buttonObj) {
+		int den1 =0;
+		int num1 = 0;
+		int den2 = 0;
+		int num2 = 0;
+		Fraction f1, f2, f3;
+		String str = "";
+
+		if (buttonObj == multiply) {
+
+			String inputError = "";
+			Boolean valid = true; 
+			
+			// Ensures all four inputs are integers
+			if (num1Field.isValidNumber()) {
+				num1 = num1Field.getNumber();
+			}
+			else {
+				inputError += "Numerator 1 must be a valid integer \n";
+				valid = false;
+			}
+			if (num2Field.isValidNumber()) {
+				num2 = num2Field.getNumber();
+			}
+			else {
+				inputError += "Numerator 2 must be a valid integer \n";
+				valid = false;
+			}
+			if (den1Field.isValidNumber()) {
+				den1 = den1Field.getNumber();
+				if(den1 == 0) {
+					inputError +="Denominator 1 cannot equal 0 \n";
+					valid = false;
+				}
+			}
+			else {
+				inputError += "Denominator 1 must be a valid integer \n";
+				valid = false;
+			}
+			if (den2Field.isValidNumber()) {
+				den2= den2Field.getNumber();
+				if(den2 == 0) {
+					inputError +="Denominator 2 cannot equal 0 \n";
+					valid = false;
+				}
+			}
+			else {
+				inputError += "Denominator 2 must be a valid integer";
+				valid = false;
+			}
+
+			if(!valid) { 	
+				messageBox(inputError);
+			}
+			else {
+				
+				f1 = new Fraction(num1, den1);
+				f2 = new Fraction(num2, den2);
+				
+				f3 = new Fraction();
+				f3.multiply(f1, f2);
+
+				str = String.format("%s * %s = %s", f1.toString(true), f2.toString(true), f3.toString(true));
+				messageBox(str);
+			}
+		}	
+	}
+
+	public static void main(String[] args) {
+		JFrame frm = new FractionUI();
+		frm.setTitle("Fraction Calculator");
+		frm.setSize(500, 300);
+		frm.setVisible(true);
+	}
 }
-
-
-
-
 
 
 
